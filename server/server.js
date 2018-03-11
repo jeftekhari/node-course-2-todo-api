7b1704faa4b9e99101ce7920c3cf9b362333202e
jeftekhari <jeftekha@student.42.us.org>
2018-03-11 14:40:21 -0700

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
-		return res.send({todo})
+		res.send({todo})
 	}).catch((e) => {
 		return res.status(400).send()
 	});
